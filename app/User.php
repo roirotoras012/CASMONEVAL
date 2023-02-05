71d21f2ef2dae0c57c9757defcaadbe91738725b
Denzi0 <darkblackx44@gmail.com>
2023-02-05 14:54:26 +0800

    added inputs on forms

--- app/User.php
+++ app/User.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    protected $fillable = [
+        'username', 
+        'email', 
+        'first_name', 
+        'last_name',
+        'middle_name',
+        'extension_name',
+        'birthday',
+        'password',
+        'user_type_ID',
+    ];
+
     use HasFactory;
 }
