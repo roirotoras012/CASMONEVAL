304ca81101258f6402608689909d68e50b4ea86e
Roi <jerelroi@apollo.com.ph>
2023-02-12 22:31:29 +0800

    initial annual target input table

--- app/Http/Controllers/RegionalDirector.php
+++ app/Http/Controllers/RegionalDirector.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\StrategicMeasure;
 
 class RegionalDirector extends Controller
 {
     public function index()
-    {
+    {   
+        $labels = StrategicMeasure::join('strategic_objectives', 'strategic_measures.strategic_objective_ID', '=', 'strategic_objectives.strategic_objective_ID')
+                            ->get(["strategic_measures.strategic_measure", "strategic_objectives.strategic_objective"]);
        
-        return view("rd.dashboard");
+        return view("rd.dashboard", compact('labels'));
     }
 }
--- resources/views/rd/dashboard.blade.php
+++ resources/views/rd/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
-
+<head>
+    <link rel="stylesheet" href="{{ asset('css/rd.css') }}">
+    <script src="{{ asset('js/custom.js') }}"></script>
+</head>
 @section('content')
     <x-user-sidebar>
         <div class="container-fluid px-4 py-5">
@@ -9,9 +12,80 @@
                 <li class="breadcrumb-item active"><h1>Regional Director Dashboard</h1></li>
                 <div><h2>Bobo Ka</h2></div>
             </ol>
-    
-        
-        </div>
+            <div class="opcr-container">
+                <h1>OPCR</h1>
+                
+                <div class="opcr-table">
 
+                  
+        
+              
+
+              
+      
+            
+                    <table>
+                        <thead>
+                          <tr>
+                           
+                            <th >Strategic Objectives</th>
+                            <th >Strategic Measures</th>
+                            <th>REGION 10</th>
+                            <th>BUK</th>
+                            <th>CAM</th>
+                            <th>LDN</th>
+                            <th>MISOR</th>
+                            <th>MISOC</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          
+                          @foreach ($labels as $label)
+                          <tr class="table-tr">
+                            @if ($label->strategic_objective)
+                                
+                            @endif
+                            <td>{{ $label->strategic_objective }}</td>
+                            <td>{{ $label->strategic_measure }}</td>
+                          
+                            <td></td>
+                            <td><input type="number"></td>
+                            <td><input type="number"></td>
+                            <td><input type="number"></td>
+                            <td><input type="number"></td>
+                            <td><input type="number"></td>
+                            
+                            </tr>
+                          @endforeach
+                       
+                            
+                           
+                          
+                              
+                               
+                          
+                            
+                            
+                      
+                           
+                            
+                         
+                        
+                       
+                       
+                        
+                          
+                          
+                          
+                        </tbody>
+                      </table>
+                </div>
+        
+        
+            </div>
+        </div>
+       
     </x-user-sidebar>
+
+   
 @endsection
