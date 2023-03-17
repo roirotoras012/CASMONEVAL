7c8d3e91d50fc740c900bd6ecc4e40660b195197
Jim Lao <laojimmarklino@gmail.com>
2023-03-17 19:47:16 +0800

    accomplishment added

---
+++ resources/views/components/update_monthly_accom_modal.blade.php
@@ -0,0 +1,52 @@
+<!-- Modal -->
+
+@props(['month', 'division_ID', 'monthly_target'])
+
+
+
+<div class="modal fade" id="<?=$month.'_'.$monthly_target?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="jan_35">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                
+                <form method="POST" action="{{ route('dc.store-accom')}}" >
+                    @csrf
+                    
+                        <input type="text" name="monthly_target_ID" value="{{$monthly_target}}">
+                        <div class="row">
+                            <label for="monthly_target"
+                                class="col-form-label text-md-start">{{ __('Monthly Accomplishment') }}</label>
+
+                            <div>
+                                <input type="text" id="monthly_accom"
+                                    class="form-control @error('monthly_accom') is-invalid @enderror"
+                                    name="monthly_accom" value="{{ old('monthly_accom') }}" required autofocus />
+
+
+                                @error('monthly_accom')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+
+                            </div>
+                        </div>
+
+
+                        <div class="d-grid col-10 mx-auto my-3">
+                            <button class="btn btn-primary" type="submit">{{ __('Add Monthly Accomplishment') }}</button>
+                        </div>
+                    </div>
+                </form>
+              
+            </div>
+            
+
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -109,8 +109,9 @@ Route::middleware(['auth', 'App\Http\Middleware\CheckRole:5'])->group(function (
   Route::get('dc/coaching', [DivisionChiefController::class, 'coaching']);
   Route::get('dc/job-fam', [DivisionChiefController::class, 'jobfam']);
   Route::get('dc/view-target', [DivisionChiefController::class, 'bukidnunBddIndex'])->name('dc.bukidnunBddIndex');
-  Route::get('dc/accomplishment', [DivisionChiefController::class, 'accomplishment']);
-  Route::post('dc/monthly_targets', [DivisionChiefController::class, 'store'])->name('dc.store');;
+  Route::get('dc/accomplishment', [DivisionChiefController::class, 'accomplishment'])->name('dc.accomplishments');;
+  Route::post('dc/monthly_targets', [DivisionChiefController::class, 'store'])->name('dc.store');
+  Route::post('dc/monthly_accomplishment', [DivisionChiefController::class, 'storeAccom'])->name('dc.store-accom');
   Route::get('dc/profile', [DivisionChiefController::class, 'profile']);
   Route::post('dc/profile/update-email', [DivisionChiefController::class, 'updateEmailHandler'])->name('dc.updateEmailHandler');
   Route::post('dc/profile/update-password', [DivisionChiefController::class, 'updatePasswordHandler'])->name('dc.updatePasswordHandler');
