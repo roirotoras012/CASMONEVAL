1c6b37a85136431e1de8d00582de2287344aae48
Denzi0 <darkblackx44@gmail.com>
2023-03-19 18:00:54 +0800

    change some design shit

--- resources/views/dc/dashboard.blade.php
+++ resources/views/dc/dashboard.blade.php
@@ -12,10 +12,10 @@
             <ol class="breadcrumb mb-4">
             
                 <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                    <div class="card-header">Header</div>
+                    <div class="card-header">Number of Targets</div>
                     <div class="card-body">
-                      <h5 class="card-title">Primary card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <h5 class="card-title">1</h5>
+                
                     </div>
                   </div>
               
