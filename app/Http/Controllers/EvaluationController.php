2a19fce2483e649bf9e419095730dd50bec00b8c
Mark Achacoso <makmakachacoso@gmail.com>
2023-03-30 19:51:54 +0800

    changes

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -64,41 +64,41 @@ class EvaluationController extends Controller
             $evaluation->remark = $request->input('remark');
             $evaluation->save();
 
-            $userName = auth()->user()->first_name;
-            $provinceID = auth()->user()->province_ID;
-            $userTypeID = auth()->user()->user_type_ID;
-            $opcr_id = $request->input('opcr_id');
-            $opcr = Opcr::find($opcr_id);
+            // $userName = auth()->user()->first_name;
+            // $provinceID = auth()->user()->province_ID;
+            // $userTypeID = auth()->user()->user_type_ID;
+            // $opcr_id = $request->input('opcr_id');
+            // $opcr = Opcr::find($opcr_id);
 
-            $data = $userName . ' has added remarks';
+            // $data = $userName . ' has added remarks';
 
-            $user_ID = Auth::id();
+            // $user_ID = Auth::id();
 
-            // Define an array of division IDs and corresponding division names
-            $divisions = [
-                1 => 'BDD',
-                2 => 'CPD',
-                3 => 'FAD',
-            ];
+            // // Define an array of division IDs and corresponding division names
+            // $divisions = [
+            //     1 => 'BDD',
+            //     2 => 'CPD',
+            //     3 => 'FAD',
+            // ];
 
-            // Loop through the division IDs and create a notification for each division
-            foreach ($divisions as $divisionID => $divisionName) {
-                $notification = new Notification([
-                    'user_type_ID' => 5, // Notify to DC
-                    'user_ID' => $user_ID,
-                    'division_ID' => $divisionID,
-                    'province_ID' => $provinceID,
-                    // 'opcr_ID' => $opcr_id,
-                    'type' => 'Division Chief Evaluation',
-                    'data' => $data,
-                ]);
+            // // Loop through the division IDs and create a notification for each division
+            // foreach ($divisions as $divisionID => $divisionName) {
+            //     $notification = new Notification([
+            //         'user_type_ID' => 5, // Notify to DC
+            //         'user_ID' => $user_ID,
+            //         'division_ID' => $divisionID,
+            //         'province_ID' => $provinceID,
+            //         // 'opcr_ID' => $opcr_id,
+            //         'type' => 'Division Chief Evaluation',
+            //         'data' => $data,
+            //     ]);
 
-                // Add the division name to the notification message
-                $message = $data . ' for ' . $divisionName;
-                $notification->data = $message;
-                dd($notification);
-                $notification->save();
-            }
+            //     // Add the division name to the notification message
+            //     $message = $data . ' for ' . $divisionName;
+            //     $notification->data = $message;
+            //     dd($notification);
+            //     $notification->save();
+            // }
 
             return redirect()
                 ->back()
