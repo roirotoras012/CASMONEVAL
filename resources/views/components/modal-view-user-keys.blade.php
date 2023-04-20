2a935e051e6d0a4f2fc035d045e5da22d4c6f377
Denzi0 <darkblackx44@gmail.com>
2023-04-20 18:07:41 +0800

    added detials for user keys

--- resources/views/components/modal-view-user-keys.blade.php
+++ resources/views/components/modal-view-user-keys.blade.php
@@ -9,18 +9,44 @@
                 </ul> --}}
             <table class="table table-bordered table-striped">
                 <tr>
+
+                    <th scope="col" class="bg-primary text-white">User Type</th>
+                    <th scope="col" class="bg-primary text-white">Province</th>
+                    <th scope="col" class="bg-primary text-white">Division</th>
                     <th scope="col" class="bg-primary text-white">UserKey</th>
                     <th scope="col" class="bg-primary text-white">Status</th>
-                       <th scope="col" class="bg-primary text-white">User ID</th>
-                    <th scope="col" class="bg-primary text-white">Division ID</th>
                 </tr>
+                @php
+                    $userTypeNames = [
+                        1 => 'Regional Director',
+                        2 => 'Regional Planning Officer',
+                        3 => 'Provincial Director',
+                        4 => 'Provincial Planning Officer',
+                        5 => 'Division Chief',
+                    ];
+                    $userProvinces = [
+                        1 => 'Bukidnon',
+                        2 => 'Lanao Del Norte',
+                        3 => 'Misamis Oriental',
+                        4 => 'Misamis Occidental',
+                        5 => 'Camiguin',
+                    ];
+                    $userDivisions = [
+                        1 => 'Business Development Division',
+                        2 => 'Consumer Protection Division',
+                        3 => 'Finance Administrative Division'
+                     
+                    ];
+                @endphp
                 @foreach ($userRegistrationKeys ?? [] as $userRegistrationKey)
                     <tr>
-
+                        <td>{{ $userTypeNames[$userRegistrationKey->user_type_ID] ?? 'N/A' }}</td>
+                        <td>{{ $userProvinces[$userRegistrationKey->province_ID] ?? 'N/A' }}</td>
+                        <td>{{ $userDivisions[$userRegistrationKey->division_ID] ?? 'N/A'}}</td>
                         <td>{{ $userRegistrationKey->registration_key }}</td>
-                        <td>{{ $userRegistrationKey->Status}}</td>
-                         <td>{{ $userRegistrationKey->user_ID}}</td>
-                        <td>{{ $userRegistrationKey->division_ID}}</td>
+                        <td
+                            class="{{ $userRegistrationKey->Status === 'Taken' ? 'bg-danger' : 'bg-success' }} text-white">
+                            {{ $userRegistrationKey->Status }}</td>
                     </tr>
                 @endforeach
             </table>
