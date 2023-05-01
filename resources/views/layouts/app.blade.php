0e193c1e60d1dcd676a00c8feec13025e3e52eef
Mark Achacoso <makmakachacoso@gmail.com>
2023-05-01 14:14:00 +0800

    add ngrok meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
     <title>@yield('title')</title>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
