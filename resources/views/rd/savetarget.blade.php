4781c6989df6e4f31ca1e6b5a7d7864f394a5ffb
Mark Achacoso <makmakachacoso@gmail.com>
2023-07-12 11:31:59 +0800

    delete cards

--- resources/views/rd/savetarget.blade.php
+++ resources/views/rd/savetarget.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('title')
-    {{ 'Regional Director Save Target' }}
+    {{ 'RD Save Target' }}
 @endsection
 @section('content')
     <x-user-sidebar>
         <div class="loading-screen">
             <img src="{{ asset('images/loading.gif') }}" alt="Loading...">
-          </div>
-          <div class="container-fluid px-4 py-5">
+        </div>
+        <div class="container-fluid px-4 py-5">
 
             <ol class="breadcrumb mb-4">
 
@@ -16,45 +16,67 @@
                 </li>
 
             </ol>
+            
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p class="m-0">{{ $message }}</p>
+            </div>
+            @endif
+            @if ($message = Session::get('error'))
+            <div class="alert alert-danger">
+                <p class="m-0">{{ $message }}</p>
+            </div>
+            @endif
             <div class="opcr-list-container">
                 @foreach ($opcr as $item)
-                    <div class="opcr-item">
-                        <ul>
+                    <div class="opcr-item" style="position:relative">
+                        <div style="position:absolute; right: 10px">
+                       
+                          
+                        </div>
+                        <ul style="margin-right: 50px">
                             <li style="list-style: none !important;">
-                                <a href="{{ url('rd/opcr/' . $item->opcr_ID) }}">
-                                    <h4>OPCR #: {{ $item->opcr_ID }}</h4>
+                                <a href="{{ url('rd/opcr/' . $item->opcr_ID) }}" style="text-decoration: none; color: black">
+                                    <li style="list-style: none !important;">
+                                        <h4>Description: {{ $item->description }}</h4>
+                                    </li>
+                                    <li style="list-style: none !important;">
+                                        <h4>Year: {{ $item->year }}</h4>
+                                    </li>
+                                    <li style="list-style: none !important;">
+                                        @if ($item->status == 'COMPLETE')
+                                            <h5
+                                                style="color: #fff; background: green; padding: 10px; border-radius: 20px; width: 180px;">
+                                                Status: {{ $item->status }}</h5>
+                                        @elseif ($item->status == 'INCOMPLETE')
+                                            <h5
+                                                style="color: #fff; background: red; padding: 10px; border-radius: 20px; width: 195px;">
+                                                Status: {{ $item->status }}</h5>
+                                        @elseif ($item->status == 'DONE')
+                                            <h5
+                                                style="color: #fff; background: green; padding: 10px; border-radius: 20px; width: 195px;">
+                                                Status: {{ $item->status }}</h5>
+                                        @else
+                                            <h4>Status: </h4>
+                                        @endif
+        
+        
+                                        @if ($item->is_active == true && $item->status != 'DONE')
+                                            <h5
+                                                style="color: #fff; background: green; padding: 10px; border-radius: 20px; width: 180px;">
+                                                Status: ACTIVE
+                                            @elseif ($item->is_active == false && $item->status != 'DONE')
+                                                <h5
+                                                    style="color: #fff; background: red; padding: 10px; border-radius: 20px; width: 195px;">
+                                                    Status: INACTIVE</h5>
+                                            @else
+                                        @endif
+        
+                                    </li>
                                 </a>
 
                             </li>
-                            <li style="list-style: none !important;">
-                                <h4>Description: {{ $item->description }}</h4>
-                            </li>
-                            <li style="list-style: none !important;">
-                                <h4>Year: {{ $item->year }}</h4>
-                            </li>
-                            <li style="list-style: none !important;">
-                                @if ($item->status == 'COMPLETE')
-                                    <h5 style="color: #fff; background: green; padding: 10px; border-radius: 20px; width: 180px;">
-                                        Status: {{ $item->status }}</h5>
-                                @elseif ($item->status == 'INCOMPLETE')
-                                    <h5 style="color: #fff; background: red; padding: 10px; border-radius: 20px; width: 195px;">Status: {{ $item->status }}</h5>
-                                @elseif ($item->status == 'DONE')
-                                    <h5 style="color: #fff; background: green; padding: 10px; border-radius: 20px; width: 195px;">Status: {{ $item->status }}</h5>
-                                @else
-                                    <h4>Status: </h4>
-                                @endif
-
-
-                                @if ($item->is_active == true && $item->status != 'DONE')
-                                <h5 style="color: #fff; background: green; padding: 10px; border-radius: 20px; width: 180px;">
-                                    Status: ACTIVE
-                                @elseif ($item->is_active == false && $item->status != 'DONE')
-                                    <h5 style="color: #fff; background: red; padding: 10px; border-radius: 20px; width: 195px;">Status: INACTIVE</h5>
-                                @else
-                                    
-                                @endif
-
-                            </li>
+                            
 
 
                         </ul>
@@ -69,6 +91,6 @@
 
 
         </div>
-
+       
     </x-user-sidebar>
 @endsection
