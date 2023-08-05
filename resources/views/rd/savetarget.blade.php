10dd9b76c2675c4c4e40f2207bf5184b3e1152c4
MarK Achacoso <makmakachacoso@gmail.com>
2023-08-05 08:53:42 +0800

    update rd

--- resources/views/rd/savetarget.blade.php
+++ resources/views/rd/savetarget.blade.php
@@ -12,88 +12,78 @@
             <ol class="breadcrumb mb-4">
 
                 <li class="breadcrumb-item active">
-                    <h2 class="text-uppercase lead  text-black p-2 rounded">RD <i class="fa-solid fa-angles-right"></i> Saved Targets</h2>
+                    <h2 class="text-uppercase lead  text-black p-2 rounded">RD <i class="fa-solid fa-angles-right"></i> Saved
+                        Targets</h2>
                 </li>
 
             </ol>
-            
+
             @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p class="m-0">{{ $message }}</p>
-            </div>
+                <div class="alert alert-success">
+                    <p class="m-0">{{ $message }}</p>
+                </div>
             @endif
             @if ($message = Session::get('error'))
-            <div class="alert alert-danger">
-                <p class="m-0">{{ $message }}</p>
-            </div>
+                <div class="alert alert-danger">
+                    <p class="m-0">{{ $message }}</p>
+                </div>
             @endif
             <div class="opcr-list-container">
-                @foreach ($opcr as $item)
-                    <div class="opcr-item" style="position:relative">
-                        <div style="position:absolute; right: 10px">
-                       
-                          
-                        </div>
-                        <ul style="margin-right: 50px">
-                            <li style="list-style: none !important;">
-                                <a href="{{ url('rd/opcr/' . $item->opcr_ID) }}" style="text-decoration: none; color: black">
-                                    <li style="list-style: none !important;">
-                                        <h4>Description: {{ $item->description }}</h4>
-                                    </li>
-                                    <li style="list-style: none !important;">
-                                        <h4>Year: {{ $item->year }}</h4>
-                                    </li>
-                                    <li style="list-style: none !important;">
-                                        @if ($item->status == 'COMPLETE')
-                                            <h5
-                                                style="color: #fff; background: green; padding: 10px 20px; border-radius: 20px; width: 280px;">
-                                                Target Status: {{ $item->status }}</h5>
-                                        @elseif ($item->status == 'INCOMPLETE')
-                                            <h5
-                                                style="color: #fff; background: red; padding: 10px 20px; border-radius: 20px; width: 280px;">
-                                                Target Status: {{ $item->status }}</h5>
-                                        @elseif ($item->status == 'DONE')
-                                            <h5
-                                                style="color: #fff; background: green; padding: 10px 20px; border-radius: 20px; width: 230px;">
-                                                OPCR Status: {{ $item->status }}</h5>
-                                        @else
-                                            <h4>Status: </h4>
-                                        @endif
-        
-        
-                                        @if ($item->is_active == true && $item->status != 'DONE')
-                                            <h5
-                                                style="color: #fff; background: green; padding: 10px 20px; border-radius: 20px; width: 280px;">
-                                                Submit Status: ACTIVE
-                                            @elseif ($item->is_active == false && $item->status != 'DONE')
-                                                <h5
-                                                    style="color: #fff; background: red; padding: 10px 20px; border-radius: 20px; width: 280px;">
-                                                    Submit Status: INACTIVE</h5>
-                                            @else
-                                        @endif
-                                       
-                                    </li>
-                                    <button class="btn btn-primary" style="border-radius: 25px; padding: 8px 30px;">
-                                        <a style="text-decoration: none; color:white;" href="{{ url('rd/opcr/' . $item->opcr_ID) }}">View OPCR</a> 
-                                    </button>
-                                </a>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Year</th>
+                            <th>OPCR Status</th>
+                            <th>Submit Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($opcr as $item)
+                            <tr>
+                                <td>{{ $item->description }}</td>
+                                <td>{{ $item->year }}</td>
+                                <td>
+                                    @if ($item->status == 'COMPLETE')
+                                        <span class="status-complete">{{ $item->status }}</span>
+                                    @elseif ($item->status == 'INCOMPLETE')
+                                        <span class="status-incomplete">{{ $item->status }}</span>
+                                    @elseif ($item->status == 'DONE')
+                                        <span class="status-done">{{ $item->status }}</span>
+                                    @else
+                                        <span>Status:</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($item->is_active == true && $item->status != 'DONE')
+                                        <span class="submit-status-active">ACTIVE</span>
+                                    @elseif ($item->is_active == false && $item->status != 'DONE')
+                                        <span class="submit-status-inactive">INACTIVE</span>
+                                    @elseif ($item->status == 'DONE')
+                                        <span class="submit-status-active">DONE</span>
+                                    @else
+                                    @endif
+                                </td>
+                                <td class="row d-flex align-items-center">
+                                    <form action="{{ route('rpo.remove_opcr') }}" method="post">
+                                        @csrf
+                                        {{-- <input type="hidden" value="{{ $item->opcr_ID }}" name="opcr_ID">
+                                        <button type="submit" class="btn btn-danger" onclick="confirmDeletion(event)"><i
+                                                class="fas fa-archive text-white"></i></button> --}}
+                                        <a class="btn btn-warning" href="{{ url('rd/opcr/' . $item->opcr_ID) }}"><i
+                                                class="fa-solid fa-eye text-white"></i></a>
+                                    </form>
 
-                            </li>
-                            
-
-
-                        </ul>
-
-
-                    </div>
-                @endforeach
-
-
-
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
 
 
         </div>
-       
+
     </x-user-sidebar>
 @endsection
