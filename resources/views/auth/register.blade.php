0d87ca0c43649de8074d0f098693abeb4ac14c09
Denzi0 <darkblackx44@gmail.com>
2023-08-29 12:09:10 +0800

    denzel-changes-final-na-gud

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,6 @@
                                         </div>
                                        
                                         <input placeholder="Please enter registraion key" id="password" type="password"
-<<<<<<< HEAD
            class="form-control eye-password @error('registration_key') is-invalid invalid-input @enderror{{ session('error') ? ' invalid-input' : '' }}"
                                             name="registration_key" autocomplete="registration_key" required
                                             pattern="^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$">
@@ -31,21 +30,6 @@
                                             <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
-                                       
-=======
-                                            class="form-control eye-password {{ session('error') ? 'invalid-input-key' : '' }}"
-                                            name="registration_key" autocomplete="registration_key" required
-                                            pattern="^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$"  oninvalid="this.setCustomValidity('Please enter a valid registration key')"
-                                            oninput="this.setCustomValidity('')">
-
-                                           
-
-                                        {{-- @error('email')
-                                            <span class="invalid-feedback" role="alert" />
-                                            <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror --}}
->>>>>>> 8f8ec200fd5bad74364a041a625f58102d7cf756
                                         <div class="input-group-append">
                                             <button class="btn btn-outline-secondary toggle-password" type="button"
                                                 id="toggle-password">
@@ -54,19 +38,11 @@
                                         </div>
                                        
                                     </div>
-<<<<<<< HEAD
                                      @if (session('error'))
                                             <small class="text-danger">
                                                 {{ session('error') }}
                                             </small>
                                         @endif
-=======
-                                    @if(session('error'))
-                                    <small class="text-danger">
-                                        {{ session('error') }}
-                                    </small>
-                                @endif
->>>>>>> 8f8ec200fd5bad74364a041a625f58102d7cf756
                                     <div class="mb-0 mt-2">
                                         <button type="submit" class="btn btn-primary w-100 d-block">
                                             {{ __('Validate') }}
